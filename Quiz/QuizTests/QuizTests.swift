e52542f0fcbe30a9a8b8ec6b070e94213538e916
Kevin Mahoney <kmahoney9709dev@gmail.com>
2024-12-18 09:58:31 -0600

    Quiz App First Commit

---
+++ Quiz/QuizTests/QuizTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuizTests.swift
+//  QuizTests
+//
+//  Created by Mahoney, Kevin (STL) on 12/18/24.
+//
+
+import Testing
+@testable import Quiz
+
+struct QuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
